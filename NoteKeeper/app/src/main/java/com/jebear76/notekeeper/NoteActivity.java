2e2cdc14e4174a47213beababebbfd002d71c7cf
JeBear76 <jerepondumie@gmail.com>
2019-04-26 18:49:00 +0200

    End of Module 3

--- NoteKeeper/app/src/main/java/com/jebear76/notekeeper/NoteActivity.java
+++ NoteKeeper/app/src/main/java/com/jebear76/notekeeper/NoteActivity.java
@@ -1,15 +1,21 @@
 package com.jebear76.notekeeper;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.List;
 
 public class NoteActivity extends AppCompatActivity {
+    public static final String NOTE_INFO = "com.jebear76.notekeeper.NOTE_INFO";
+    private NoteInfo noteInfo;
+    private boolean isNewNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,30 @@ public class NoteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_note);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Spinner spinnerCourses = (Spinner) findViewById(R.id.spinner_courses);
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+        ArrayAdapter<CourseInfo> adapterCourses = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, courses);
+        adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCourses.setAdapter(adapterCourses);
+
+        readDisplayStateValues();
+
+        EditText textTitle = findViewById(R.id.editText_note_title);
+        EditText textBody = findViewById(R.id.editText_note_body);
+
+        if (!isNewNote) {
+            textTitle.setText(noteInfo.getTitle());
+            textBody.setText(noteInfo.getBody());
+
+            int courseIndex = courses.indexOf(noteInfo.getCourse());
+            spinnerCourses.setSelection(courseIndex);
+        }
+    }
+
+    private void readDisplayStateValues() {
+        Intent intent = getIntent();
+        noteInfo = intent.getParcelableExtra(NoteActivity.NOTE_INFO);
+        isNewNote = noteInfo == null;
     }
 
     @Override
