4800cfc54ee06669ce5c92bea6151cc9d5cd1247
JeBear76 <jerepondumie@gmail.com>
2019-05-03 14:39:03 +0200

    Course 3 Module 5 End

---
+++ NoteKeeper/app/src/main/java/com/jebear76/notekeeper/CourseRecyclerAdapter.java
@@ -0,0 +1,55 @@
+package com.jebear76.notekeeper;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class CourseRecyclerAdapter extends RecyclerView.Adapter<CourseRecyclerAdapter.ViewHolder>{
+
+    private final Context _context;
+    private final LayoutInflater _layoutInflater;
+    private List<CourseInfo> _courseInfoList;
+
+    public CourseRecyclerAdapter(Context context, List<CourseInfo> courseInfoList) {
+        _context = context;
+        _courseInfoList = courseInfoList;
+        _layoutInflater = LayoutInflater.from(_context);
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View itemView = _layoutInflater.inflate(R.layout.item_course_list, viewGroup, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+        CourseInfo courseInfo = _courseInfoList.get(position);
+        viewHolder._textCourse.setText(courseInfo.getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return _courseInfoList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public final TextView _textCourse;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            _textCourse = itemView.findViewById(R.id.text_course);
+        }
+
+    }
+}
--- NoteKeeper/app/src/main/java/com/jebear76/notekeeper/MainActivity.java
+++ NoteKeeper/app/src/main/java/com/jebear76/notekeeper/MainActivity.java
@@ -3,6 +3,7 @@ package com.jebear76.notekeeper;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -21,6 +22,10 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     private NoteRecyclerAdapter _noteRecyclerAdapter;
+    private RecyclerView _recyclerView;
+    private LinearLayoutManager _linearLayoutManager;
+    private CourseRecyclerAdapter _courseRecyclerAdapter;
+    private GridLayoutManager _gridLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,14 +70,34 @@ public class MainActivity extends AppCompatActivity
 
     private void initializeDisplayContent() {
 
-        final RecyclerView recyclerNotes = (RecyclerView) findViewById(R.id.list_notes);
-        final LinearLayoutManager notesLayoutManager = new LinearLayoutManager(this);
-        recyclerNotes.setLayoutManager(notesLayoutManager);
+        _recyclerView = findViewById(R.id.list_notes);
+        _linearLayoutManager = new LinearLayoutManager(this);
+        _gridLayoutManager = new GridLayoutManager(this,2);
 
         List<NoteInfo> noteInfoList = DataManager.getInstance().getNotes();
         _noteRecyclerAdapter = new NoteRecyclerAdapter(this,noteInfoList);
-        recyclerNotes.setAdapter(_noteRecyclerAdapter);
+        List<CourseInfo> courseInfoList = DataManager.getInstance().getCourses();
+        _courseRecyclerAdapter = new CourseRecyclerAdapter(this, courseInfoList);
+        displayNotes();
 
+    }
+
+    private void displayCourses() {
+        _recyclerView.setLayoutManager(_gridLayoutManager);
+        _recyclerView.setAdapter(_courseRecyclerAdapter);
+        setNavMenuSelection(R.id.nav_courses);
+    }
+
+    private void setNavMenuSelection(int menu_item_id) {
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        Menu menu = navigationView.getMenu();
+        menu.findItem(menu_item_id).setChecked(true);
+    }
+
+    private void displayNotes() {
+        _recyclerView.setLayoutManager(_linearLayoutManager);
+        _recyclerView.setAdapter(_noteRecyclerAdapter);
+        setNavMenuSelection(R.id.nav_notes);
     }
 
     @Override
@@ -114,9 +139,9 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_notes) {
-            // Handle the camera action
+            displayNotes();
         } else if (id == R.id.nav_courses) {
-
+            displayCourses();
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
